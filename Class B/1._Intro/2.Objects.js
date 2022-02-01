d39b574ac9bda3ed247a8550861960916c075872
Mick <mich8g56@stud.kea.dk>
2022-02-01 19:38:18 +0100

    mere Lir

--- Class B/1._Intro/2.Objects.js
+++ Class B/1._Intro/2.Objects.js
@@ -8,18 +8,26 @@ const alienMessage = {
 };
 
 // Log the message 
-
+console.log("Exercise 1: ", alienMessage.message);
+console.log("");
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
-
-
+const myObject = {
+	name: "Michael",
+	age: 30,
+	secret: "Please dont tell people im 30"
+}
+console.log("Exercise 2: ",myObject);
+console.log("");
 // --------------------------------------
 // Exercise 3 - Add a property 
 
 const stackOverflow = {};
-
+stackOverflow.isAllowed = true
+console.log("Exercise 3: ", stackOverflow);
+console.log("");
 // make a rule called isAllowed and let the value be true
 
 // --------------------------------------
@@ -30,8 +38,12 @@ const thisSong = {
 }
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
+delete thisSong.description
+thisSong.about = "Just a tribute."
+thisSong.beYouAngels = false
+thisSong.weAreButMen = true
 
-
+console.log("Exercise 4: ", thisSong);
 // --------------------------------------
 
 
