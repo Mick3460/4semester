63ab97bafefc7e8df1de4149ed8d47ba6626a0b4
Mick <mich8g56@stud.kea.dk>
2022-02-08 20:46:43 +0100

    Node CRUD homework done

--- Class B/2._First_Express_Server/app.js
+++ Class B/2._First_Express_Server/app.js
@@ -1,3 +1,4 @@
+ 
  //import Express
 const express = require('express')
 const app = express();
@@ -6,12 +7,9 @@ const app = express();
 
 app.use(express.json()); // allow to parse JSON
 
-
 // takes an endpoint and a callback function
 app.get("/", (req, res) => {
     res.send({message: "Hello world. Express converts this Javascript object to JSON" });
-
-
 });
 
 //todo implement a route called welcome that welcomes the client
@@ -19,11 +17,84 @@ app.get("/welcome",(req,res) => {
     res.send({message: "Hejsa"});
 });
 
-app.post("/mirror",(req,res) => {
-    res.send(req.body);
+
+
+//
+// EXERCISE / HOMEWORK 
+//
+const beerArray = [{id: 1, name: "Tuborg"},{id:2, name: "Carlsberg"}]
+let highestId = 0
+
+//Find highest Id
+beerArray.forEach(beer => {
+    if (beer.id >= highestId){
+        highestId = (beer.id)+1 
+    }
+});
+
+//assigns ID and updates highest ID number
+function assignId () {
+    const theNewId = highestId
+    highestId +=1
+    return theNewId
+}
+
+//GET
+app.get("/beers",(req,res) => {
+    res.send(beerArray);
+})
+//GET by id
+app.get("/beers/:beerId",(req,res) => {
+    res.send(beerArray[(req.params.beerId)-1]);
 })
 
+//POST and assign id
+app.post("/beers",(req,res) => {
+    const newBeer = {id: assignId(), name: req.body.name}
+   
+    beerArray.push(newBeer)
+    res.send(newBeer);
+})
 
+//PUT 
+app.put("/beers/:beerId",(req,res) => {
+    const targetId = parseFloat(req.params.beerId)
+    const indexNumber = beerArray.findIndex( beer => beer.id === targetId) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+    //note to self, findIndex(element, index, array), (element,index) or (element) 
+    const beerReplace = req.body  
+    if (indexNumber !== -1) {
+        beerReplace.id = targetId //adjust body.id to URL id, if body is incorrect
+        beerArray[indexNumber] = beerReplace
+    }
+
+    res.send(beerReplace); 
+})
+
+//PATCH
+app.patch("/beers/:beerId",(req,res) => {
+    const targetId = parseFloat(req.params.beerId)
+    beerArray.forEach(beer => {
+        if (beer.id === targetId){
+            if (beer.name !== req.body.name && req.body.name !== undefined){
+                beer.name = req.body.name
+            }
+            if (beer.age !== req.body.age && req.body.age !== undefined){
+                console.log("No age key, but just an example of how to do it with multiple key-value-pairs");
+            }
+        }
+        
+    });
+
+    res.send(beerArray);
+})
+
+//DELETE
+app.delete("/beers/:beerId",(req,res) => {
+    const targetId = parseFloat(req.params.beerId)
+    const indexNumber = beerArray.findIndex( beer => beer.id === targetId)
+    beerArray.splice(indexNumber,1)
+    res.send(beerArray); //return array to check
+})
 
 
 
