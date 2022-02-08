b17e5a95eb0b3395165c54edc69311300993bd6d
Mick <mich8g56@stud.kea.dk>
2022-02-08 21:30:16 +0100

    notes fixed.. still need to work on id for hte project

--- Class B/2._First_Express_Server/app.js
+++ Class B/2._First_Express_Server/app.js
@@ -3,21 +3,7 @@
 const express = require('express')
 const app = express();
 
-// const app = require("express")()   I EN ONELINER SMIL
-
 app.use(express.json()); // allow to parse JSON
-
-// takes an endpoint and a callback function
-app.get("/", (req, res) => {
-    res.send({message: "Hello world. Express converts this Javascript object to JSON" });
-});
-
-//todo implement a route called welcome that welcomes the client
-app.get("/welcome",(req,res) => {
-    res.send({message: "Hejsa"});
-});
-
-
 
 //
 // EXERCISE / HOMEWORK 
