222819292847715b1cc6de2128a239483b6fc16a
Mick <mich8g56@stud.kea.dk>
2022-03-02 09:39:52 +0100

    sending to stationairy

---
+++ Class B/3._World_wide_express/routers/football.js
@@ -0,0 +1,12 @@
+
+// WE WONT NEED THIS DOCUMENT, BUT HERE IT IS LOL
+
+const player = "Messi";
+const football = true;
+
+//new syntax for inserting a key value pair into an object.
+const sausage = {
+    player,
+    football,
+
+}
