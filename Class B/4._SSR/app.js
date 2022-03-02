5548e27994f843ed80f82b9f190c3cb759326514
Mick <mich8g56@stud.kea.dk>
2022-03-02 22:21:07 +0100

    næsten færdig

--- Class B/4._SSR/app.js
+++ Class B/4._SSR/app.js
@@ -1,35 +1,78 @@
 const express = require('express');
 const app = express();
-
-app.use(express.static("public")); 
-//the public folder is sent to the client.
-
-// no need for app.use(express.json()) because we aren't going to have json/data
-
-
-// create the frontpage amd the themepark pages and serve them with express
-
 const fs = require('fs'); //file system import
+// no need for app.use(express.json()) because we aren't going to have json/data
+app.use(express.static("public"));   //the public folder is open/(sent?) to the client.
 
+//Structural data
+const header = fs.readFileSync("./public/components/header/header.html").toString();
+const nav = fs.readFileSync("./public/components/nav/nav.html").toString(); // nav is now a bitestream (maybe just a buffer? what is a buffer?), so we input .tostring
 const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
-const nav = fs.readFileSync("./public/components/nav/nav.html").toString();
-// nav is now a bitestream, so we input .tostring
-const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
-const frontpagePage = nav.replace("%%TITLE REPLACE","Title has been replaced")+frontpage+footer;
 
+
+
+
+//Frontpage
+const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
+const frontpagePage = header.replace("%%CSSPATH","./components/nav/global.css")+
+                        nav.replace("%%TITLE REPLACE","Title has been replaced")+frontpage+footer;
 
 app.get("/", (req,res) => {
     res.send(frontpagePage)
 })
 
-app.get( "/123", (req,res) => {
-    res.sendFile(__dirname+"/public/pages/frontpage/frontpage.html")
+//INTRO NOTES
+const introNotes = fs.readFileSync("./public/pages/introNotes/intronotes.html").toString();
+const introNotesPage = header.replace("%%CSSPATH","./pages/introNotes/intronotes.css") + 
+                        nav.replace("%%TITLE REPLACE","Intro notes page")+introNotes+footer;
+
+app.get("/intronotes", (req,res) => {
+    res.send(introNotesPage)
 })
 
-const themeparkP = fs.readFileSync("./public/pages/themepark/themepark.html").toString();
-const themeparkPage = nav.replace("%%TITLE REPLACE", "det her er themepark replacement")  +themeparkP+footer
-app.get("/themepark", (req,res) => {
-    res.send(themeparkPage)
+//FIRST EXPRESS SERVER
+const expressNotes = fs.readFileSync("./public/pages/firstExpressServer/firstExpressServer.html").toString();
+const expressNotesPage = header.replace("%%CSSPATH","./pages/firstExpressServer/firstExpressServer.css")   +
+                            nav.replace("%%TITLE REPLACE","First Express Server") + expressNotes + footer;
+
+app.get("/firstexpressserver", (req,res) => {
+    res.send(expressNotesPage)
+})
+
+//WWExpress and/or HTML express
+const wwExpress = fs.readFileSync("./public/pages/wwExpress/wwExpress.html").toString();
+const wwExpressPage = header.replace("%%CSSPATH","./pages/wwExpress/wwExpress.css")   +
+                            nav.replace("%%TITLE REPLACE","World wide Express") + wwExpress + footer;
+
+app.get("/wwexpress", (req,res) => {
+    res.send(wwExpressPage)
+})
+
+//Server-side rendering notes
+const ssrNotes = fs.readFileSync("./public/pages/serverSideRendering/serverSideRendering.html").toString();
+const ssrNotesPage = header.replace("%%CSSPATH","./pages/serverSideRendering/serverSideRendering.css")   +
+                            nav.replace("%%TITLE REPLACE","Server side rendering") + ssrNotes + footer;
+
+app.get("/ssr", (req,res) => {
+    res.send(ssrNotesPage)
+})
+
+//Express example
+const expressExample = fs.readFileSync("./public/pages/expressBackendExample/expressBackendExample.html").toString();  
+const expressExamplePage = header.replace("%%CSSPATH","./pages/expressBackendExample/expressBackendExample.css")   +
+                            nav.replace("%%TITLE REPLACE","Express Backend Example") + expressExample + footer;
+
+app.get("/expressbackend", (req,res) => {
+    res.send(expressExamplePage)
+})
+
+//Git and terminal commands.
+const gitCommands = fs.readFileSync("./public/pages/gitcommands/gitcommands.html").toString();   
+const gitCommandsPage = header.replace("%%CSSPATH","./pages/gitcommands/gitcommands.css")   +
+                            nav.replace("%%TITLE REPLACE","Terminal commands") + gitCommands + footer;
+
+app.get("/terminalcommands",(req,res) =>{
+    res.send(gitCommandsPage)
 })
 
 
@@ -37,7 +80,7 @@ app.get("/themepark", (req,res) => {
 
 
 
-const PORT = 9090;
+const PORT = process.env.PORT || 9000
 app.listen(PORT, () => {
     console.log("Running on PORT:",PORT);
 });
