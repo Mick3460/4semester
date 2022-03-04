6907efee6ead7ff764183debdcfdac68ce2b76fe
Mick <mich8g56@stud.kea.dk>
2022-03-04 19:10:51 +0100

    router tilføjet

---
+++ Class B/4._SSR/public/routers/routerExample/routerExample.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const fs = require('fs'); //file system import
+
+//Structural data
+const header = fs.readFileSync("./public/components/header/header.html").toString();
+const nav = fs.readFileSync("./public/components/nav/nav.html").toString(); // nav is now a bitestream (maybe just a buffer? what is a buffer?), so we input .tostring
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+
+//Routing page construction
+const routerpag = fs.readFileSync("./public/routers/routerexample/routerexample.html").toString();
+const routerPage = header.replace("%%CSSPATH","./components/nav/global.css")+
+                        nav.replace("%%TITLE REPLACE","Just a routing test")+routerpag+footer;
+
+router.get("/routertest", (req,res) => {
+    res.send(routerPage)
+})
+/* plain html file send
+router.get("/routertest",(req,res) => {
+
+    res.sendFile(__dirname + "/routerExample.html")
+})
+*/
+module.exports = {
+    //router: router
+    router
+}
